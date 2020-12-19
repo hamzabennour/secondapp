be4599fd5c5d904d127184e2f2177e70899144e6
Ibrahim.H <bitsnaps@yahoo.fr>
2020-12-19 12:50:30 +0100

    generate tasks controller

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -40,7 +40,7 @@ AppAsset::register($this);
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Tasks', 'url' => ['/site/task']],
+            ['label' => 'Tasks', 'url' => ['/tasks/index']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
