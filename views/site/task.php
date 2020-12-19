2cce058f7ac4ae0d38e88cff618b9b66fd936cd1
hamzabennour <js_bennour@esi.dz>
2020-12-19 12:39:50 +0100

    add table tasks

--- views/site/task.php
+++ views/site/task.php
@@ -2,10 +2,22 @@
 
 <p>Today is: <?= $today ?></p>
 
-<p>Nbr of Tasks <?= count($tasks) ?></p>
+<p>Number of Tasks <?= count($tasks) ?></p>
 
 <!-- TODO: List of Tasks -->
-<table>
+<table class="table table-hover">
+<thead>
   <tr>
+  <th>ID </th>
+  <th>task </th>
+  <th>done </th>
   </tr>
+</thead>
+  <?php foreach( $tasks as $task){ ?>
+    <tr>
+    <td><?=$task->id?></td>
+    <td><?=$task->task?></td>
+    <td><?=$task->done?></td>
+    </tr>
+  <?php } ?>
 </table>
