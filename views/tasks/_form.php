5ed595b19748ed3ce47c244aaf4a90447e2e4e87
hamzabennour <js_bennour@esi.dz>
2020-12-19 14:16:53 +0100

    add CRUD actions

---
+++ views/tasks/_form.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Tasks */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="tasks-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'task')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'done')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
